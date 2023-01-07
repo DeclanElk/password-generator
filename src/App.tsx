4ddf41c024c57adeab3bd88683694ee0b6bbe1e4
DeclanElk <delkerton2001@gmail.com>
2023-01-07 23:51:48 +1000

    Adding UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import './index.css'
+import { Card, 
+  Switch, 
+  FormGroup, 
+  FormControlLabel, 
+  Slider, 
+  TextField, 
+  Button 
+} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Password Generator</h1>
+      <h4>
+        Created by <a href="https://github.com/DeclanElk">DeclanElk</a>
+      </h4>
+      <Card
+      sx = {{
+        width: '90%',
+        margin: '0 auto',
+        maxWidth: '900px',
+      }}>
+        <FormGroup>
+          <FormControlLabel control={<Switch defaultChecked />} label="Uppercase" />
+          <FormControlLabel control={<Switch defaultChecked />} label="Lowercase" />
+          <FormControlLabel control={<Switch defaultChecked />} label="Numbers" />
+          <FormControlLabel control={<Switch defaultChecked />} label="Symbols" />
+          <Slider
+            defaultValue = {15}
+            min = {5}
+            max = {30}
+            step = {1}
+            marks
+            valueLabelDisplay = "auto"
+            aria-label = "Password length" 
+            sx = {{
+              width: "90%",
+              margin: "0 auto"
+            }}
+          />
+          <TextField 
+            id = "outlined-basic" 
+            variant = "outlined" 
+            value = {"test val"} 
+            InputProps = {{
+              readOnly: true,
+            }}
+            aria-readonly />
+          <Button>Generate</Button>
+        </FormGroup>
+      </Card>
     </div>
   );
 }
