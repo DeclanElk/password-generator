38c36af65b51fe6bc3a58b915661039a67514a8f
DeclanElk <delkerton2001@gmail.com>
2023-01-13 23:32:19 +1000

    Adding unit tests for disabling of button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("All character set options render", () => {
@@ -20,4 +21,28 @@ test("Password generates on render", () => {
 
     const passwordOutput = screen.getByTestId("passwordField");
     expect(passwordOutput).not.toHaveValue("")
+})
+
+test("Generate button active when charset options selected", () => {
+    render(<App />)
+
+    const generateButton = screen.getByTestId("generateButton");
+    expect(generateButton).toBeEnabled()
+})
+
+test("Generate button disabled when no charset options selected", () => {
+    render(<App />)
+
+    const lowerCharset = screen.getByTestId("lowerSwitch");
+    const upperCharset = screen.getByTestId("upperSwitch");
+    const numbersCharset = screen.getByTestId("numbersSwitch");
+    const symbolsCharset = screen.getByTestId("symbolsSwitch");
+
+    userEvent.click(lowerCharset)
+    userEvent.click(upperCharset)
+    userEvent.click(numbersCharset)
+    userEvent.click(symbolsCharset)
+
+    const generateButton = screen.getByTestId("generateButton");
+    expect(generateButton).not.toBeEnabled()
 })
